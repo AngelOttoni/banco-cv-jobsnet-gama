67374e0cbeba423cfff4aaa0b65b78087f39b757
AngelOttoni <angel.m.ottoni@gmail.com>
2021-08-29 17:04:54 -0300

    reorganizando as pastas

--- front-end/myscript.js
+++ front-end/myscript.js
@@ -92,7 +92,7 @@ const New_form = () => {
 }
 
 const createCandidate = async (candidate) => {
-    const requisicao = await fetch('http://localhost:3000/register', {
+    const requisicao = await fetch('http://localhost:8081/register', {
         method: "POST",
         headers: {
             'Accept': 'application/json',
